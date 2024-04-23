65a55b190d8b90aec20c6e2e346fe43449b50f66
ianvergara <vianj26@gmail.com>
2024-04-23 12:39:24 +0800

    Add matrix transpose functionality

--- main.cpp
+++ main.cpp
@@ -17,8 +17,14 @@ int main()
         {4,5,6},
         {7,8,9}
     };
+
+    std::vector<std::vector<int>> matrix_C = 
+    {
+        {1,2,3},
+        {4,5,6}
+    };
     
-    std::vector<std::vector<int>> result = matrix::subtract(matrix_A, matrix_B);
+    std::vector<std::vector<int>> result = matrix::transpose(matrix_C);
 
 
     matrix::print_matrix(result);
--- matrix_utils.h
+++ matrix_utils.h
@@ -6,6 +6,8 @@ namespace matrix
 {
     void print_matrix(std::vector<std::vector<int>> matrix);
 
+    std::vector<std::vector<int>> transpose(std::vector<std::vector<int>> matrix);
+
     std::vector<std::vector<int>> add(std::vector<std::vector<int>> matrix_A, std::vector<std::vector<int>> matrix_B);
 
     std::vector<std::vector<int>> subtract(std::vector<std::vector<int>> matrix_A, std::vector<std::vector<int>> matrix_B);
