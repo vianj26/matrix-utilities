eec969a92239665a485429af837ed7be22f0f2a4
ianvergara <vianj26@gmail.com>
2024-04-28 15:35:11 +0800

    Made variadic function solve to avoid object copying

--- eq_solver.cpp
+++ eq_solver.cpp
@@ -1,3 +1,19 @@
 #include "eq_solver.h"
 
-//variadic template
+std::unordered_map<std::string, std::vector<double>> Solver::columns{};
+
+
+/*
+void Solver::solve(std::vector<Equation>& list)
+{
+    for(auto& equation : list)
+    {
+        std::cout << equation.equation << std::endl;
+        for(auto& member : equation.values)
+        {
+
+            (columns[member.first]).push_back(member.second);
+        }
+    }
+}
+*/
